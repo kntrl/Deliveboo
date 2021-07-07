db782058ea6d8b5d734415f53d18614f1d64d1e0
Lorenzo <lorypsy@gmail.com>
2021-07-07 16:59:09 +0200

    Remove: all files regarding test api. Moved to test-front branch.

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
-Route::get('/categories','Api\TestCategoryController@index')->name('categoryList');
-
-Route::get('/{category}','Api\TestCategoryController@show')->name('restaurantsByCategory');
