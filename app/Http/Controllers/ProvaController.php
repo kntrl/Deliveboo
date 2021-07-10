175db2fe80255e8be63610a1943cad1ec2b87491
Lorenzo <lorypsy@gmail.com>
2021-07-10 20:24:45 +0200

    TEST ADD: Query to get orders from a User.

--- app/Http/Controllers/ProvaController.php
+++ app/Http/Controllers/ProvaController.php
@@ -4,19 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
-
+use App\Order;
 class ProvaController extends Controller
 {
     public function index()
     {
         $orders = DB::table('orders')
+            ->select('orders.id','foods.name','food_order.quantity')
             ->join('food_order', 'orders.id', '=', 'food_order.order_id')
             ->join('foods', 'foods.id', '=', 'food_order.food_id')
-            /* ->join('users', 'foods.user_id', '=', 'users.id') */
-
-            ->where('user_id', '=', '1')
-
+            ->where('foods.user_id', '=', '3')
             ->get();
+
 
         return view('viewProva', compact('orders'));
     }
