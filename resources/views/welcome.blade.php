0a38d44e44d32b73058804684710740c97a037b8
OwlBot92 <48355693+OwlBot92@users.noreply.github.com>
2021-07-12 19:39:51 +0200

    add: struttura nei file .blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,109 +11,125 @@
 
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
         <!-- Styles --> 
-        {{-- <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            ul {
-                list-style-type: none;
-                padding: 0;
-            }
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style> --}}
+        
 
     </head>
 
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Dashboard</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+        
+        @include('partials/header')
+        
+        <div class="my-jumbotron">
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                
-                <div class="my-jumbotron">
-                    <h1>DELIVEBOO</h1>
-                </div>
-                
-
-                <main class="main-content">
-                    <div class="flexable-container">
-                        @for ($i = 0; $i < 5; $i++)
-                            <div class="my-card">
-                                <div class="inner-card">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-primary">See Restaurants</a>
-                                </div>
-                            </div>
-                        @endfor
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-md-6 col-lg-6 col-xl-6 left-side">
+                        <h1>Ordina i tuoi piatti preferiti direttamente dal tuo divano</h1>
+                        <p>Piatti tipici da tutto il mondo, direttamente a casa tua</p>
+                        <input type="text" name="" id="" placeholder="Cosa vorresti mangiare?">
                     </div>
-                </main>
-                
-
-
+                    
+                    <div class="col-12 col-md-6 col-lg-6 col-xl-6 right-side">
+                        <img src="{{asset("img/undraw_Hamburger_8ge6.png")}}" alt="">
+                    </div>
+                </div>
             </div>
+
+
         </div>
 
+
+
+        <div class="content">
+            <main class="main-content">
+                <div class="container">
+                    {{-- ROW 1 --}}
+                    <div class="row">
+                        <div class="col-sm-12 col-md-6 col-lg-3 card-temp">
+                            <div class="inner-box">
+                                test
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-md-6 col-lg-3 card-temp">
+                            <div class="inner-box">
+                                test
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-md-6 col-lg-3 card-temp">
+                            <div class="inner-box">
+                                test
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-md-6 col-lg-3 card-temp">
+                            <div class="inner-box">
+                                test
+                            </div>
+                        </div>
+                    </div>
+
+                    {{-- ROW 2 --}}
+                    <div class="row">
+                        <div class="col-sm-12 col-md-6 col-lg-3 card-temp">
+                            <div class="inner-box">
+                                test
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-md-6 col-lg-3 card-temp">
+                            <div class="inner-box">
+                                test
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-md-6 col-lg-3 card-temp">
+                            <div class="inner-box">
+                                test
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-md-6 col-lg-3 card-temp">
+                            <div class="inner-box">
+                                test
+                            </div>
+                        </div>
+                    </div>
+
+                    {{-- ROW 3 --}}
+                    <div class="row">
+                        <div class="col-sm-12 col-md-6 col-lg-3 card-temp">
+                            <div class="inner-box">
+                                test
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-md-6 col-lg-3 card-temp">
+                            <div class="inner-box">
+                                test
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-md-6 col-lg-3 card-temp">
+                            <div class="inner-box">
+                                test
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-md-6 col-lg-3 card-temp">
+                            <div class="inner-box">
+                                test
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+    
+        
+
+        @include('partials/footer')
+
+
+
+        
+        {{-- gsap --}}
         <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.1/gsap.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.1/ScrollTrigger.min.js"></script>
+
+        {{-- my script --}}
         <script src="{{ asset("js/gsapAnimations.js") }}"></script>
     </body>
 </html>
