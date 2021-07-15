5dc061df7869eaf61e8d71825f7ad54d63e3cd6f
Lorenzo <lorypsy@gmail.com>
2021-07-15 20:58:34 +0200

    UPDATE: improved restaurant by category json resp

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 use App\Category;
 
 use App\Http\Controllers\Controller;
+use Braintree\Address;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -23,9 +24,29 @@ class CategoryController extends Controller
     {
         $restaurants = $category->users;
 
+        foreach ($restaurants as $element) {
+
+            foreach ($element->categories as $category) {
+                $elementCategories[] = [
+                    'name' => $category->name,
+                    'slug' => $category->slug,
+                ];
+            }
+            
+            $restaurant = [
+                'id' => $element->id,
+                'name' => $element->name,
+                'slug' => $element->slug,
+                'address' => $element->address,
+                'description' => $element->description,
+                'piva' => $element->piva,
+                'categories' => $elementCategories
+            ];  
+            $responseRestaurants[]= $restaurant;
+        }
+
         $response = [
-            'category' => $category->name,
-            'restaurants' => $restaurants
+            'restaurants' => $responseRestaurants
         ];
 
         return response()->json($response);
