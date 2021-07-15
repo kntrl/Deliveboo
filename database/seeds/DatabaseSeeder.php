995c24ad4757c3b341c927835e07bfe0d93b3e35
karin valcanover <valcanoverkarin@outlook.it>
2021-07-15 11:15:25 +0200

    update database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call([UsersTableSeeder::class, 
+        CategoriesTableSeeder::class, 
+        FoodsTableSeeder::class, 
+        OrdersTableSeeder::class, 
+        FoodOrderTableSeeder::class, 
+        RestaurantsCategoryTableSeeder::class]);
     }
 }
