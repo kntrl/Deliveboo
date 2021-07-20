2129189372dfd4759759bb2b8c7ab9e4c9abcaa7
Lorenzo <lorypsy@gmail.com>
2021-07-20 17:31:28 +0200

    ADD: Email Verify view.

---
+++ resources/views/email/verify.blade.php
@@ -0,0 +1,8 @@
+<img src="{{asset('img/logo.png')}}" alt="Deliveboo - Logo" width="200px">
+
+<h1>Verifica la tua mail</h1>
+
+<p>Per completare la registrazione è necessario verificare la tua email <a href="{{$url}}">cliccando qui</a> .</p>
+<p>Qualora il link non funzionasse copia e incolla questo indirizzo nel tuo broswer : </p>
+
+{{$url}}
