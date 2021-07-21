20dd41341365d3fa68ebb36aaf6a47d01b0b155f
Arianna Carotti <arianna.carotti@gmail.com>
2021-07-21 13:15:59 +0200

    sistemato alcune media query

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,9 +35,9 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     <img src="{{ asset("/img/logo.png") }}" alt="">
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                {{-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
-                </button>
+                </button> --}}
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
