182b643380cd83da58ef084f44a113b4b08a73e6
Arianna Carotti <arianna.carotti@gmail.com>
2021-07-21 17:02:46 +0200

    cominciato modifiche navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,9 +30,9 @@
 
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+        <nav class="navbar-expand-md navbar-light shadow-sm app-navbar">
+            <div class="app-navbar-container">
+                <a class="navbar-logo" href="{{ url('/') }}">
                     <img src="{{ asset("/img/logo.png") }}" alt="">
                 </a>
                 {{-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -72,9 +72,12 @@
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
-                                    </form>
-                                </div>
+                                    </form>                           
+                                </div>                              
                             </li>
+
+                            
+
                         @endguest
                     </ul>
 
